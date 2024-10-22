b1dd49b5127f1231296e981ec5f16d15d52fdfc5
Dr-Laika <jsdetlefs@gmail.com>
2024-10-22 16:12:44 +0200

    Fix .bat files hopefully

--- launcher.bat
+++ launcher.bat
@@ -1,3 +1,4 @@
-.\.venv\Scripts\python.exe -m pip install --upgrade pip %*
-.\.venv\Scripts\python.exe .\prompt_handler_SporkV2.py %*
+.\.venv\Scripts\activate %*
+pip install --upgrade pip %*
+python.exe prompt_handler_SporkV2.py %*
 pause
